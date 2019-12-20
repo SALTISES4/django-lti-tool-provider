6c40e7cab6b33aa5ba17b063bab6152671082d11
Antoine Lefebvre-Brossard <antoinelb@protonmail.com>
2019-12-20 13:26:17 -0500

    Working dummy request validator

--- django_lti_tool_provider/request_validator.py
+++ django_lti_tool_provider/request_validator.py
@@ -1,7 +1,12 @@
+from django.conf import settings
 from oauthlib.oauth1 import RequestValidator as _RequestValidator
 
 
 class RequestValidator(_RequestValidator):
+    @property
+    def enforce_ssl(self):
+        return False
+
     def validate_timestamp_and_nonce(
         self,
         client_key,
@@ -17,4 +22,7 @@ class RequestValidator(_RequestValidator):
         return True
 
     def get_client_secret(self, client_key, request):
-        return "tmp"
+        return unicode(settings.LTI_CLIENT_SECRET)
+
+    def check_client_key(self, client_key):
+        return True
